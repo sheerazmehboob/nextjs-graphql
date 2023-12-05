44718d6fb119bacd874adf703c534f381d1539c5
sheerazmehboob <sherazmehboob14@gmail.com>
2023-12-06 00:15:04 +0500

    Setup Sever

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.listen(process.env.PORT || 5000, ()=> {
+    console.log(`Listening on PORT ${process.env.PORT}`);
+})
